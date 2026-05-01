1e087cf3a4e68a50c324b71acc12714aabfd69d5
Bhoomi Haresh Hemani <bhoomihemani02@gmail.com>
2026-05-01 19:23:48 +0530

    Initial Spring Boot setup with JSP

---
+++ src/main/java/com/bhoomi/sga2/Sga2Application.java
@@ -0,0 +1,13 @@
+package com.bhoomi.sga2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sga2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sga2Application.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+    <h1>Hello Spring Boot JSP is working</h1>
+</body>
+</html>
